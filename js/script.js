b237924f658b797ca53e0c61ee7b5374b4ca6e62
GavinZura1 <gazu9487@colorado.edu>
2025-08-08 16:14:04 -0500

    Added VERY BASIC functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
 const headerEl = document.getElementById('dynamic-head');
+const searchInputEl = document.getElementById('search-input');
+const validStateEl = document.getElementById('valid-state');
+const imgEl = document.getElementById('image');
 
-function changeHeader() {
-    const inputValue = document.getElementById('search-input').value;
-    
-    headerEl.textContent = inputValue || "Hello World!";
-    console.log(inputValue);
+const images = {
+    cat: "img/cat.jpeg",
+    dog: "img/dog.webp",
+}
+
+function checkIfValid() {
+    const searchValue = searchInputEl.value.trim();
+    const lowerCaseValue = searchValue.toLowerCase();
+    //console.log(lowerCaseValue);
+
+    // if (lowerCaseValue === 'valid') {
+    //     validStateEl.textContent = 'Valid';
+    //     validStateEl.style.color = 'green';
+    // } else if (lowerCaseValue === '') {
+    //     validStateEl.textContent = '';
+    //     validStateEl.style.color = '';
+    // } else {
+    //     validStateEl.textContent = 'Invalid';
+    //     validStateEl.style.color = 'red';
+    // }   
+
+    valid = false;
+    for (const key in images) {
+        if (lowerCaseValue === key) {
+            valid = true;
+        }
+    }
+    if (valid) {
+        validStateEl.textContent = 'Valid';
+        validStateEl.style.color = 'green';
+        imgEl.src = images[lowerCaseValue];
+    } else if (lowerCaseValue === '') {
+        validStateEl.textContent = '';
+        validStateEl.style.color = '';
+        imgEl.src = '';
+    } else {
+        validStateEl.textContent = 'Invalid';
+        validStateEl.style.color = 'red';
+        imgEl.src = '';
+    }
 }
